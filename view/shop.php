4871547b4395f00001df66c801b204b64350375e
jefflopo <jeffersonlopo@gmail.com>
2017-02-07 23:12:59 -0200

    Adicionado arquivo imagens/produtos e shop.php com as configuracoes

---
+++ view/shop.php
@@ -0,0 +1,43 @@
+<?php 
+	include_once("header.php");
+?>
+		<section>
+			
+			<div class="container">
+
+				<div class="destaque-produtos">
+					<div class="item">
+					
+						<div class="col-sm-6">
+							<img src="imagens/produtos/moto-x.png" alt="Nome do Aparelho">
+						</div>
+
+						<div class="col-sm-6">
+							<h2>Smartphone Motorola Moto X Play Dual Chip Desbloqueado Android 5.1</h2>
+
+							<div class="box-valor">
+								<div class="text-noboleto">no boleto</div>
+								<div class="text-por">por</div>
+								<div class="text-reais">R$</div>
+								<div class="text-valor">1.259</div>
+								<div class="text-valor-centavos">,10</div>
+								<div class="text-parcelas">ou em até 8x de R$ 174,88</div>
+								<div class="text-total">total a prazo R$1.399,00</div>
+								<a href="#" class="btn btn-comprar"><i class="fa fa-shopping-cart"></i>compre agora</a>
+							</div>
+							
+						</div>
+
+					</div>
+				</div>
+
+				<button type="button" id="btn-destaque-prev"><i class="fa fa-angle-left"></i></button>
+				<button type="button" id="btn-destaque-next"><i class="fa fa-angle-right"></i></button>
+				
+			</div>
+
+		</section>
+
+		<?php 
+			include_once("footer.php");
+		?>
