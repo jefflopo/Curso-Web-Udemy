bcc46c1e9049ddf6c8c77edad68b068849a0980d
jefflopo <jeffersonlopo@gmail.com>
2017-03-06 11:53:30 -0300

    Arquivo configuration.php atualizado

--- inc/configuration.php
+++ inc/configuration.php
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-<?php 
-session_start();
-
-class Sql {
-
-	public $conn;
-
-	public function __construct(){
-
-		return $this->conn = mysqli_connect("127.0.0.1", "root", "root", "hcode_shop");
-
-	}
-
-	public function query($string_query){
-
-		return mysqli_query($this->conn, $string_query);
-
-	}
-
-	public function select($string_query){
-
-		$result = $this->query($string_query);
-
-		$data = array();
-
-	    while ($row = mysqli_fetch_array($result)) {
-	        
-	    	foreach ($row as $key => $value) {
-	    		$row[$key] = utf8_encode($value);
-	    	}
-
-	        array_push($data, $row);
-
-	    }
-
-	    unset($result);
-
-	    return $data;
-
-	}
-
-	public function __destruct(){
-
-		mysqli_close($this->conn);
-
-	}
-
-}
-
-=======
 <?php
 	class Sql{
 
@@ -56,7 +5,7 @@ class Sql {
 
 		public function __construct(){
 
-			return $this->conn = mysqli_connect("127.0.0.1", "root", "admin", "hcode_shop");
+			return $this->conn = mysqli_connect("127.0.0.1", "root", "", "hcode_shop");
 
 		}
 
@@ -97,5 +46,4 @@ class Sql {
 		}
 
 	}
->>>>>>> b59888b35be9fbcd2b1e966395eb66cc2f7f38d8
 ?>
